8058042d2cba3319431fbbb93439ec092b7f1fd7
JollyCookie2000 <xam_comico@simplelogin.co>
2024-06-04 19:21:32 +0200

    Check and enable extensions

--- Test/main.cpp
+++ Test/main.cpp
@@ -1,6 +1,10 @@
 #define _CRT_SECURE_NO_WARNINGS // Disable annoying Visual Studio error about strncpy
 
+#include <algorithm>
 #include <iostream>
+#include <string>
+#include <string.h> // std::strncpy
+#include <vector>
 
 #include <openxr/openxr.h>
 
@@ -16,12 +20,57 @@ int main()
 	application_info.engineVersion = 0;
 	application_info.apiVersion = XR_API_VERSION_1_0;
 
+	// Not sure this is needed
+	uint32_t available_api_layers_count = 0;
+	xrEnumerateApiLayerProperties(0, &available_api_layers_count, nullptr);
+	std::vector<XrApiLayerProperties> available_api_layers;
+	available_api_layers.resize(available_api_layers_count, { XR_TYPE_API_LAYER_PROPERTIES });
+	xrEnumerateApiLayerProperties(available_api_layers_count, &available_api_layers_count, available_api_layers.data());
+	std::cout << "Available layers:" << std::endl;
+	for (const auto &api_layer : available_api_layers)
+	{
+		std::cout << " - " << api_layer.layerName << std::endl;
+	}
+
+	// List available extensions
+	uint32_t available_extensions_count = 0;
+	xrEnumerateInstanceExtensionProperties(nullptr, 0, &available_extensions_count, nullptr);
+	std::vector<XrExtensionProperties> available_extensions;
+	available_extensions.resize(available_extensions_count, { XR_TYPE_EXTENSION_PROPERTIES });
+	xrEnumerateInstanceExtensionProperties(nullptr, available_extensions_count, &available_extensions_count, available_extensions.data());
+	std::cout << "Available extensions:" << std::endl;
+	for (const auto& extension : available_extensions)
+	{
+		std::cout << " - " << extension.extensionName << std::endl;
+	}
+
+	// Configure extensions
+	std::vector<const char*> extensions;
+	extensions.push_back(XR_EXT_DEBUG_UTILS_EXTENSION_NAME);
+	extensions.push_back("XR_KHR_opengl_enable");
+	std::cout << "Enabled extensions:" << std::endl;
+	for (const auto& extension : extensions)
+	{
+		std::cout << " - " << extension << std::endl;
+	}
+
+	// Make sure the needed extensions are actually available
+	for (const std::string enabled_extension : extensions)
+	{
+		if (std::find_if(available_extensions.begin(), available_extensions.end(), [&enabled_extension] (const XrExtensionProperties &available_extension) { return enabled_extension == available_extension.extensionName; }) == available_extensions.end())
+		{
+			std::cerr << "[ERROR] Extension \"" << enabled_extension << "\" is not available." << std::endl;
+			return 1;
+		}
+	}
+
 	// https://registry.khronos.org/OpenXR/specs/1.0/man/html/XrInstanceCreateInfo.html
 	XrInstanceCreateInfo instance_create_info = {};
 	instance_create_info.type = XR_TYPE_INSTANCE_CREATE_INFO;
 	instance_create_info.createFlags = NULL;
 	instance_create_info.enabledApiLayerCount = 0;
-	instance_create_info.enabledExtensionCount = 0;
+	instance_create_info.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
+	instance_create_info.enabledExtensionNames = extensions.data();
 	instance_create_info.applicationInfo = application_info;
 
 	// https://registry.khronos.org/OpenXR/specs/1.0/man/html/XrInstance.html
@@ -49,6 +98,11 @@ int main()
 	xrGetInstanceProperties(instance, &instance_properties);
 	std::cout << "[INFO] Runtime name: " <<	instance_properties.runtimeName << std::endl;
 	std::cout << "[INFO] Runtime version: " << XR_VERSION_MAJOR(instance_properties.runtimeVersion) << "." << XR_VERSION_MINOR(instance_properties.runtimeVersion) << "." << XR_VERSION_PATCH(instance_properties.runtimeVersion) << std::endl;
+
+
+
+	// Get the System ID
+
 
 
 
